6cfa56b957bc2e80680e011bb29c6b3a88cca73c
AZIZBEK <pubgmobile2006xxl@gmail.com>
2026-06-13 22:19:24 +0500

    dcfvf

--- vite-project/src/Companents/Layout/Layout.jsx
+++ vite-project/src/Companents/Layout/Layout.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar'
+import Section1 from '../Main/Section1/Section1'
 
 const Layout = () => {
   return (
     <>
     <Navbar></Navbar>
+    <Section1></Section1>
     </>
   )
 }
---
+++ vite-project/src/Companents/Main/Section1/Section1.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import grp from '../../img/Group93.png'
+const Section1 = () => {
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default Section1
